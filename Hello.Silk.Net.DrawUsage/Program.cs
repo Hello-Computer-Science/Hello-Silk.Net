ea5818a2f206a508152f5932daf973a513337e49
Dynesshely <catrol@qq.com>
2023-04-19 17:33:51 +0800

    💾 📝 📄 Feat, Chore, Docs: About draw usage.

---
+++ Hello.Silk.Net.DrawUsage/Program.cs
@@ -0,0 +1,51 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
+using System.Drawing;
+
+Console.WriteLine("Hello, Silk.Net!");
+
+var _options = WindowOptions.Default with
+{
+    Size = new Vector2D<int>(800, 600),
+    Title = "Hello.Silk.Net.DrawUsage"
+};
+
+var _window = Window.Create(_options);
+
+GL? _gl = null;
+
+_window.Load += () =>
+{
+    Console.WriteLine("Window loaded!");
+
+    var _input = _window.CreateInput();
+
+    for (int i = 0; i < _input.Keyboards.Count; i++)
+        _input.Keyboards[i].KeyDown += (keyboard, key, keyCode) =>
+        {
+            Console.WriteLine($"Keyboard {i} key down! Key: {key}, Key code: {keyCode}");
+
+            if (key == Key.Escape)
+                _window.Close();
+        };
+
+    _gl = _window.CreateOpenGL();
+
+    _gl?.ClearColor(Color.CornflowerBlue);
+};
+
+_window.Update += (frameTime) =>
+{
+    Console.WriteLine($"Window updated! Frame time: {frameTime}");
+};
+
+_window.Render += (dateTime) =>
+{
+    Console.WriteLine($"Window rendered! Date time: {dateTime}");
+
+    _gl?.Clear(ClearBufferMask.ColorBufferBit);
+};
+
+_window.Run();
---
+++ Hello.Silk.Net.DrawUsage/README.md
@@ -0,0 +1,14 @@
+﻿# About
+
+This folder contains a simple demo that shows how to update background of a window in Silk.Net.
+
+# How to run
+
+In current folder, run `dotnet run` command.
+
+# Preview
+
+<br>
+
+![demo.png](https://raw.githubusercontent.com/Dynesshely/Hello-Silk.Net/main/Hello.Silk.Net.DrawUsage/demo.png)
+
